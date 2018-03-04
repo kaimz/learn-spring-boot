a71f5fd6dab260b625ca6af1e2326c91af0e2dca
”kaim <kaim@qnight.cn>
2018-03-04 09:07:43 +0800

    加入 mongodb 的依赖

--- src/test/java/com/wuwii/testmongodb/PetDaoTest.java
+++ src/test/java/com/wuwii/testmongodb/PetDaoTest.java
@@ -2,8 +2,11 @@ package com.wuwii.testmongodb;
 
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,19 +18,22 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.DEFAULT)
 public class PetDaoTest {
 
     @Autowired
     private PetDao petDao;
 
-    @Test
-    public void testFind() {
+    private Pet pet;
+
+    @Before
+    public void befor() {
+        pet = new Pet();
+        pet.setId(1L);
+        pet.setName("Tom");
+        pet.setSpecies("cat");
     }
 
-    @Test
-    public void testFindAll() {
-
-    }
 
     @Test
     public void testAdd() {
@@ -36,16 +42,29 @@ public class PetDaoTest {
         pet.setName("Tom");
         pet.setSpecies("cat");
         petDao.add(pet);
+    }
+
+    @Test
+    public void testFind() {
         Assert.assertThat(pet, Matchers.equalTo(petDao.find(pet.getId())));
     }
 
     @Test
-    public void testUpdate() {
-
+    public void testFindAll() {
+        System.out.println(petDao.findAll());
     }
 
     @Test
     public void testDelete() {
+        petDao.delete(pet.getId());
+        Assert.assertThat(null, Matchers.equalTo(petDao.find(pet.getId())));
+    }
 
+    @Test
+    public void testUpdate() {
+        testAdd();
+        pet.setName("KronChan");
+        petDao.update(pet);
+        Assert.assertThat(pet, Matchers.equalTo(petDao.find(pet.getId())));
     }
 }
